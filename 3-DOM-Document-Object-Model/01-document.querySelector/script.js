6b9aad235da79e5392913a95c6ea7525aae7226f
kabir0x17 <kabirj2310@gmail.com>
2022-01-28 12:05:28 +0530

    Create script.js

---
+++ 3-DOM-Document-Object-Model/01-document.querySelector/script.js
@@ -0,0 +1,11 @@
+//----------------------------//
+
+
+// 19. document.querySelector()
+
+const heading = document.querySelector('#h1');
+heading.innerText = 'Hello World';
+
+console.log(heading);
+
+//----------------------------//
