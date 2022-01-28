9324297d2fa48d2cc705704edea2b4ee5d4b5e68
kabir0x17 <kabirj2310@gmail.com>
2022-01-28 12:05:10 +0530

    Update script.js

--- 2-Language-Fundamentals/13-Variable-Shadowing/script.js
+++ 2-Language-Fundamentals/13-Variable-Shadowing/script.js
@@ -4,10 +4,8 @@
 
 // Shadowing is the process of creating a variable with the same name as a variable in a parent scope.
 
-
-// give me a example of shadowing in javascript 
 const myAge = 30;
- 
+
 function myFunction() {
   const myAge = 40;
   console.log(myAge);
